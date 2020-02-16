ca846d358d84546e0f553d52d948bcbce0d5aac6
League Student <league@iMac-15.attlocal.net>
2020-02-16 15:55:30 -0800

    almost done with level 4

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -1,4 +1,5 @@
 package _01_introduction_to_encapsulation;
+import static org.junit.Assert.assertEquals;
 
 /*
  * Encapsulation is a way of protecting the data in a class from being
@@ -23,12 +24,47 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	int itemsReceived; // must not be negative. All negative arguments get set to 0
 	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
 	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
 	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
-	
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 	}
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+
+	public void setItemsReceived(int itemsReceived) {
+		if (itemsReceived>0) {
+			
+		
+		this.itemsReceived = itemsReceived;
+		}
+	}
+
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+
+	public void setDegreesTurned(float degreesTurned) {
+		this.degreesTurned = degreesTurned;
+	}
+
+	public String getNomenclature() {
+		return nomenclature;
+	}
+
+	public void setNomenclature(String nomenclature) {
+		this.nomenclature = nomenclature;
+	}
+
+	public Object getMemberObj() {
+		return memberObj;
+	}
+
+	public void setMemberObj(Object memberObj) {
+		this.memberObj = memberObj;
+	}
+	
 }
---
+++ src/_01_introduction_to_encapsulation/Test.java
@@ -0,0 +1,11 @@
+package _01_introduction_to_encapsulation;
+import static org.junit.Assert.assertEquals;
+public class Test {
+public static void main(String[] args) {
+	
+}
+ {
+
+	
+}
+}
